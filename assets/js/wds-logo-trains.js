dd0681a16978a25f98b967c6e13b1af037a2035f
Gary Kovar <gkovar@gmail.com>
2017-01-16 19:44:20 -0500

    Adds noise making js

--- assets/js/wds-logo-trains.js
+++ assets/js/wds-logo-trains.js
@@ -2,4 +2,19 @@
 	$( '.wds-logo-train a[href="#"]' ).on( 'click', function( e ) {
 		e.preventDefault();
 	} );
+
+	$( document ).ready( function () {
+		var audioElement = document.createElement( 'audio' );
+		audioElement.setAttribute( 'src', audioFile );
+		$.get();
+		audioElement.addEventListener( "load", function () {
+			audioElement.play();
+		}, true );
+
+		$( '.button-primary' ).on( 'click', function () {
+			audioElement.play();
+		} );
+
+	} );
+
 })(jQuery);
