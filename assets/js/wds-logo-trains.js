28d2a8a24b1b18929a7edc79a0c0cf70a7f66ad7
Gary Kovar <gkovar@gmail.com>
2017-01-16 20:06:24 -0500

    Plays a train sound on save/update of CPT.

--- assets/js/wds-logo-trains.js
+++ assets/js/wds-logo-trains.js
@@ -3,18 +3,4 @@
 		e.preventDefault();
 	} );
 
-	$( document ).ready( function () {
-		var audioElement = document.createElement( 'audio' );
-		audioElement.setAttribute( 'src', audioFile );
-		$.get();
-		audioElement.addEventListener( "load", function () {
-			audioElement.play();
-		}, true );
-
-		$( '.button-primary' ).on( 'click', function () {
-			audioElement.play();
-		} );
-
-	} );
-
 })(jQuery);
